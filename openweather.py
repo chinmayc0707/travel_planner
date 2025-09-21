3961e39c8f1b2cc71410b2429817fede1b1ce96e
Chinmay C Bhat <139462368+chinmayc0707@users.noreply.github.com>
2025-09-21 17:31:01 +0530

    Add files via upload

---
+++ openweather.py
@@ -0,0 +1,23 @@
+def get_weather_data(CITY):
+    """Fetches current weather data for a specified city using OpenWeatherMap API.
+    Returns:
+        str: Current temperature in Celsius.
+    """
+    import requests
+
+    # Replace with your API key and city
+    API_KEY = "8636db0878ce04062720e92752dc34f5"
+    
+
+    # API request URL
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+    # Send request
+    response = requests.get(url)
+
+    # Check response status
+    if response.status_code == 200:
+        data = response.json()
+        temp_celsius = data['main']['temp']
+        return str(temp_celsius)
+    
